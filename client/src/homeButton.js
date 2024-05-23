85c994727acbd8d63da19549ec590cc472390df7
praneeth724 <tmkpraneeth@std.appsc.sab.ac.lk>
2024-05-23 07:38:54 +0530

    add home button

---
+++ client/src/homeButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./homeButton.css";
+
+const HomeButton = () => {
+  return <button className="homeButton">&#9664; Home</button>;
+};
+
+export default HomeButton;
